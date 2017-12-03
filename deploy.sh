198749c9c5a51d20d7e1f263b2507f0394ace976
Lee Robinson <lrobinson2011@gmail.com>
2017-12-03 08:44:06 -0600

    Remove unused font and cleanup CSS Hot fix for resume displaying weird inside Firefox Removed broken Facebook link

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
+# This script rebuilds my Hugo site and commits
+# the changes inside of the /public folder. I have a 
+# separate repository where the deployed site lives.
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+hugo
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
 cd public
-# Add changes to git.
 git add .
 
-# Commit changes.
 msg="Rebuilding portfolio: `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
 cd ..
